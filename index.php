ac9155caed162b2d5e9cd690fa8f855b9c26a433
FayadIdriss <fayadbacari-dev@outlook.com>
2024-11-20 19:43:49 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+   $recipeTitle = 'Cassoulet';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Affichage des recettes</title>
+    <link href="./style.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Host+Grotesk:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="div-body">
+       <div class="banner">
+          <div class="banner_div">
+            <i class="fa-solid fa-phone"></i>
+            <p class="banner_p">06 12 34 56 78</p>
+          </div>
+          <div class="banner_div2">
+            <i class="fa-solid fa-envelope"></i>
+            <p class="banner_p">fayad-idriss.bacari@outlook.com</p>
+          </div>
+          <div class="banner_div3">
+            <i class="fa-solid fa-map-pin"></i>
+            <p class="banner_p">56 rue du marais Villeurbanne</p>
+          </div>
+       </div>
+    </div>
+</body>
+</html>
